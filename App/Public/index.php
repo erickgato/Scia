4c8b85444442756f74096d5e7959c94fc596c016
erickgato <itsbk0703@gmail.com>
2020-06-14 14:30:50 -0300

    Adicionado identação ao html de login

--- App/Public/index.php
+++ App/Public/index.php
@@ -13,30 +13,29 @@
 		?>
 		<script src="Resources/js/estilos.js"></script>
 	</header>
-	<div class="painel">
-		<form action="login.php" method="post">
-			<label class="titulo">
-				Logar
-			</label>
-			<br/>
-			<br/>
-			<input type="hidden" name="codtpusr" value="2">
-			<label class="subtitulo">
-				CPF
-			</label>
-			<input class="caixa" type="text" name="usu_cpf" required placeholder = "Digite Seu CPF" onkeydown="javascript: fMasc( this, mCPF )" maxlength="14"/>
-			<label class="subtitulo">
-				Senha
-			</label>
-			<input class="caixa" type="password" password name="usu_pass" required placeholder = "Digite Sua Senha"/>
-			<br/>
-			<input type="submit" value="Entrar" class="botao" name='btn_enviar'/>
-		</form>
-		<?php
+	<main>
+		<div class="painel">
+		<div class="Server-Message">
+			<?php
 				if(isset($_GET['message'])){
 					echo "Usuario e / ou senha não existe";
-				}
-		?>
-	</div>
+					}
+				?>
+		</div>
+			<form action="login.php" method="post">
+				<h2 class="titulo">
+					Logar
+				</h2>
+				<!-- Input para o login.php indicando que nesta página só podem ser cadastrados
+				Usuarios do tipo 2(Responsáveis) -->
+				<input type="hidden" name="codtpusr" value="2">
+				<label class="subtitulo"> CPF  </label>
+				<input class="caixa" type="text" name="usu_cpf" required placeholder = "Digite Seu CPF" onkeydown="javascript: fMasc( this, mCPF )" maxlength="14"/>
+				<label class="subtitulo">Senha</label>
+				<input class="caixa" type="password" password name="usu_pass" required placeholder = "Digite Sua Senha"/>
+				<input type="submit" value="Entrar" class="botao" name='btn_enviar'/>
+			</form>
+		</div>
+	</main>
 </body>
 </html>
