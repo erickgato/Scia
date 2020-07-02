44c3c5dcfe1a695f27bcf6dc5528b498bda5ff9c
Erick De Souza Gato <60188768+erickgato@users.noreply.github.com>
2020-07-02 03:05:19 -0300

    Verificação de sessão adicionada

--- App/Public/View/Responsaveis/autorizaaluno.php
+++ App/Public/View/Responsaveis/autorizaaluno.php
@@ -1,37 +1,41 @@
 <!DOCTYPE html>
 <html lang="Pt-Br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="<?php echo RESOCS; ?>/css/estilos.css" /> 
+    <link rel="stylesheet" type="text/css" href="<?php echo RESOCS; ?>/css/estilos.css" />
     <title>Cadastro</title>
 </head>
+
 <body class="corpo">
     <header id="header">
-       <?php  
-           include_once RESOCS . '/Cabecalhos/Menu.php';
-            $username = $_SESSION['USERNAME'];
-            MENU::RESPONSAVEL(0,"index.php");
-            $WHERE = "WHERE AL_codResponsavel = {$_SESSION['USERID']}";
+        <?php
+        if(!isset($_SESSION['USERLOGGED'])){
+            header("Location: index.php");            
+        }  
+            include_once RESOCS . '/Cabecalhos/Menu.php';
+            $username = $_SESSION['USER']['name'];
+            MENU::RESPONSAVEL(0);
+            $WHERE = "WHERE AL_codResponsavel = {$_SESSION['USER']['ID']}";
             $FETCHALUN = DATABASE::SELECT('sc_aluno',$WHERE);
        ?>
     </header>
     <div id="container">
-        <script src="../../Resources/js/estilos.js"></script>
         <form action="" method="post">
             <div class="painel">
-            <label class="title_aut">
-				Autorizar Aluno
-			</label>
-                <div id = "campos_cadastro">
+                <label class="title_aut">
+                    Autorizar Aluno
+                </label>
+                <div id="campos_cadastro">
                     <label class="subtitulo">
-                            Aluno
-                            <select class="caixa" id="input-text" type="text" value="" name="codaluno" required> 
-                                <?php 
+                        Aluno
+                        <select class="caixa" id="input-text" type="text" value="" name="codaluno" required>
+                            <?php 
                                     if($FETCHALUN == false){
                                         echo "<script>alert('Usuario náo possui alunos cadastrados')</script>";
-                                     //   header('Location: index.php');
+                                        header('Location: perfil');
                                     }
                                     else{
                                         foreach ($FETCHALUN as $Aluno) {
@@ -40,27 +44,27 @@
                                     }
                                            
                                     ?>
-                            </select>
-                            <br/>
+                        </select>
+                        <br />
                     </label>
-                            
+
                     <label class="subtitulo">
-                            Unidade
-                            <select class="caixa" id="input-text" type="text" value="" name="unidade"> 
-                                <option  value="1">SESI</option>
-                                <option  value="2">SENAI</option>
-                            </select>
-                            <br/>
+                        Unidade
+                        <select class="caixa" id="input-text" type="text" value="" name="unidade">
+                            <option value="1">SESI</option>
+                            <option value="2">SENAI</option>
+                        </select>
+                        <br />
                     </label>
 
                     <label class="subtitulo">
                         Observações</label>
-                            <textarea name="Obs" class="textareaaluno" cols="5" rows="3"></textarea>
+                    <textarea name="Obs" class="textareaaluno" cols="5" rows="3"></textarea>
                 </div>
-            <input type="submit" class="botao" name="Enviar"/>
-        </div>
-    </form>
-    <?php 
+                <input type="submit" class="botao" name="Enviar" />
+            </div>
+        </form>
+        <?php 
     include PUBLICPATH .  '/View/Responsaveis/POSTDATA/autorizaaluno.php';
     if(isset($_GET['m'])){
         switch($_GET['m']){
@@ -74,6 +78,7 @@
     }
 
     ?>
-    <footer></footer>
+        <footer></footer>
 </body>
+
 </html>
